5cf5a84115bd7ad7ef6534f35cd64edd6aa29b23
such283 <chitumallasupradeep@gmail.com>
2025-09-16 21:20:06 +0530

    feat: implemented buffer queue

---
+++ include/buffer.hpp
@@ -0,0 +1,48 @@
+//
+// Created by Supradeep Chitumalla on 16/09/25.
+//
+
+#ifndef BUFFER_H
+#define BUFFER_H
+#include <atomic>
+#include <optional>
+
+namespace deribit {
+    template<typename T>
+    class Buffer {
+    private:
+        std::vector<T>buffer_;
+        size_t capacity_;
+        alignas(64) std::atomic<size_t> head_;
+        alignas(64) std::atomic<size_t> tail_;
+
+    public:
+        explicit Buffer(size_t capacity) : capacity_(capacity),buffer_(capacity),head_(0), tail_(0) {
+
+        }
+        bool push(const T& item ) {
+            size_t head = head_.load(std::memory_order_relaxed);
+            size_t next_head = (head+1)%capacity_;
+            if (next_head==tail_.load(std::memory_order_acquire)) {
+                return false;
+            }
+            buffer_[head] = item;
+            head_.store(next_head, std::memory_order_release);
+            return true;
+        }
+        std::optional<T> pop() {
+            size_t tail = tail_.load(std::memory_order_relaxed);
+
+            if (tail == head_.load(std::memory_order_acquire)) {
+                // Buffer empty
+                return std::nullopt;
+            }
+
+            T item = buffer_[tail];
+            tail_.store((tail + 1) % capacity_, std::memory_order_release);
+            return item;
+        }
+    };
+}
+
+#endif //BUFFER_H
